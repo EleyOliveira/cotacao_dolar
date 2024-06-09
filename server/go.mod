22e861377b4762fb27a036d69055567585b57881
EleyOliveira <eleyoliveira@bol.com.br>
2024-06-09 10:04:14 -0300

    inclusão dos arquivos do server

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/EleyOliveira/cotacao_dolar
+
+go 1.22.0
---
+++ server/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/cotacao", BuscaCotacaoHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCotacaoHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+}
